5d13a6871e070923693144033d439f46b69975b0
HaoZhu02 <zhuh2@oregonstate.edu>
2023-10-07 21:43:59 -0700

    Interval Input for Study and Break Time
    Created a component for interval settings

--- smarter-together/src/App.js
+++ smarter-together/src/App.js
@@ -2,11 +2,21 @@ import React, { useState, useRef } from 'react';
 import NumUsers from './Components/NumUsers';
 import TaskCardContainer from './Components/TaskCardContainer';
 import { sortHighLow } from './ProcessData/SortComponents';
+import IntervalSettings from './Components/IntervalSettings';
 
 function App() {
   const [numUsers, setNumUsers] = useState(0);
-  const taskCardContainerRefs = useRef([]);
+  const [breakInterval, setBreakInterval] = useState(300);
+  const [studyInterval, setStudyInterval] = useState(1200);
+  const handleBreakIntervalChange = (newBreakInterval) => {
+    setBreakInterval(newBreakInterval);
+  };
+  const handleStudyIntervalChange = (newStudyInterval) => {
+    setStudyInterval(newStudyInterval);
+  };
 
+  const taskCardContainerRefs = useRef([]);
+  
   const logAllTaskData = () => {
     const newData = [];
     for (let i = 0; i < numUsers; i++) {
@@ -23,13 +33,25 @@ function App() {
 
   return (
     <div className='NumberUsersInput'>
+      
       {/* get NumUsers input*/}
       <NumUsers setNumUsers={setNumUsers} />
+
+
       {/* dynamic TaskCardContainer based on numUsers*/}
       {[...Array(numUsers)].map((_, index) => (
         <TaskCardContainer key={index} id={`taskCardContainer${index}`} ref={(ref) => (taskCardContainerRefs.current[index] = ref)} />
       ))}
       <button onClick={logAllTaskData}>Log All Task Data</button>
+
+      <IntervalSettings 
+        breakInterval={breakInterval}
+        studyInterval={studyInterval}
+        onBreakIntervalChange={handleBreakIntervalChange}
+        onStudyIntervalChange={handleStudyIntervalChange}
+        
+      />
+      
     </div>
   );
 }
